1475a7ebcf8f4b735a0e4e7a5740b668ab0bb25f
TomaszKowal <kowal.tomaszk@gmail.com>
2016-01-10 22:30:03 +0100

    up

--- jdraw/jdraw_lextab.py
+++
@@ -1,9 +0,0 @@
-# jdraw_lextab.py. This file automatically created by PLY (version 3.4). Don't edit!
-_tabversion   = '3.2'
-_lextokens    = {'false': 1, 'JDSWINGOBJECT': 1, 'TEXT': 1, 'SYMBOL': 1, 'GLOBAL': 1, 'NUMBER': 1, 'LBRACKET': 1, 'JDPOLYLINE': 1, 'JDSPLINE': 1, 'JDIMAGE': 1, 'JDAXIS': 1, 'JDLINE': 1, 'JDGROUP': 1, 'COMMA': 1, 'JDBAR': 1, 'JDSLIDER': 1, 'JDLABEL': 1, 'JDELLIPSE': 1, 'true': 1, 'JDRECTANGLE': 1, 'TWOP': 1, 'RBRACKET': 1, 'JDFILE': 1, 'JDROUNDRECTANGLE': 1}
-_lexreflags   = 0
-_lexliterals  = ''
-_lexstateinfo = {'INITIAL': 'inclusive'}
-_lexstatere   = {'INITIAL': [('(?P<t_SYMBOL>[a-zA-Z_][a-zA-Z_0-9]*)|(?P<t_NUMBER>[-+]?[0-9]*\\.?[0-9]+([eE][-+]?[0-9]+)?)|(?P<t_newline>\\n+)|(?P<t_TEXT>\\"[^"]*\\")|(?P<t_LBRACKET>\\{)|(?P<t_COMMA>\\,)|(?P<t_TWOP>\\:)|(?P<t_RBRACKET>\\})', [None, ('t_SYMBOL', 'SYMBOL'), ('t_NUMBER', 'NUMBER'), None, ('t_newline', 'newline'), (None, 'TEXT'), (None, 'LBRACKET'), (None, 'COMMA'), (None, 'TWOP'), (None, 'RBRACKET')])]}
-_lexstateignore = {'INITIAL': ' \t'}
-_lexstateerrorf = {'INITIAL': 't_error'}
